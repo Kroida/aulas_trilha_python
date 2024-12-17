c2bad69056063031b795f8a10d2300d707c27a2f
luis <luisGNS8022@gmail.com>
2024-12-17 15:50:30 -0300

    Aula 33

---
+++ aula33/layout_place.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+def criar_interface_place():
+    # janela_place = tk.Toplevel(root)
+    janela_place = tk.Tk()
+    janela_place.title("Gerenciador place() 📍")
+    janela_place.geometry("300x300")
+    janela_place.configure(bg='lightblue')
+    
+    # Criando widgets com place()
+    tk.Label(janela_place, text="Posição Absoluta", bg="orange", fg="black").place(x=50, y=50, width=200, height=30)
+    
+    tk.Label(janela_place, text="Posição Relativa", bg="cyan", fg="black").place(x=50, y=100, width=200, height=30)
+
+    tk.Button(janela_place, text="Fechar", command=janela_place.destroy).place(x=100, y=200)
+    
+    return janela_place
+
+if __name__ == '__main__':
+    janela = criar_interface_place()
+    janela.mainloop()
